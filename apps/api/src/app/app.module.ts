b09a18f94a0778f8d9d6edb4b05962c992285289
Bram Borggreve <beeman@beeman.nl>
2023-05-14 18:32:05 +0100

    feat: add basic GraphQL setup

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,21 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'node:path'
+import { cwd } from 'node:process'
 
 import { AppController } from './app.controller'
+import { AppResolver } from './app.resolver'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(cwd(), 'api-schema.graphql'),
+      driver: ApolloDriver,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppResolver, AppService],
 })
 export class AppModule {}
